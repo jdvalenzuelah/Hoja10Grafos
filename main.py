d4058c82b9dd21d568e990ac38067ec8b22eb5fe
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-05-18 15:13:42 -0600

    Editando el Main

--- main.py
+++ main.py
@@ -5,38 +5,48 @@
 #Marcos Gutierrez 17
 #Fernando Hengstenberg 17699
 #Base de Datos y Grafos
-#******************************************************
 
-print ("************************menu************************")
-print ("1. ingresar nuevo Doctor")
-print ("2. ingresar nuevo Paciente")
-print ("3. Doctores con tipos de Especialidades")
-print ("4. una persona conoce a otra")
-print ("5. salir")
-print ("****************************************************")
+from CreateDB import *
 
-menu=input("")
+#Contar para el ciclo
+opcion = 0
 
-while menu != "salir" or menu != "5":
+print("Bienvenido al sistema de control del Hospital")
+print("¿Que desea hacer?\n 1. Ingresar nuevo doctor\n 2. Ingresar nuevo paciente\n 3. Doctores con Especialidades\n 4.Una Persona Conoce a Otra\n 5. Recomendaciones de un Paciente\n 6.Recomendaciones de un doctor \n 7. Salir")
+opcion = int(input("¿Cual es opcion a escoger?"))
 
-    if menu != "1" and menu != "2" and menu != "3" and menu != "4" and menu != "5":
-        print ("intente de nuevo")
-        menu = input("")
+#Ciclo para realizar el menu
+while opcion !=5:
+    #Opcion para agregar nuevo doctor
+    if(opcion == 1):
+        print("*****USTED INGRESARA UN NUEVO DOCTOR*****")
+        nombre = raw_input("Ingrese el nombre del doctor") 
+        colegiado = raw_input("Ingrese el numero del colegiado del doctor")
+        especialidad = raw_input("Ingrese la especialidad del doctor")
+        telefono = raw_input("Ingrse el nuero de telefono del doctor")
+        #Llamamos a la funcion agregarDoctor
+        agregarDoctor(nombre, colegiado, especialidad, telefono)
+
+    #Opcion para agregar un nuevo paciente
+    if(opcion == 2):
+        print("*****USTED INGRESARA UN NUEVO PACIENTE")
+        nombrePaciente = raw_input("Ingrese el nombre del paciente")
+        telefonoPaciente = raw_input("Ingrese el numero telefonico del paciente")
+        #Llamamos la funcion agregarPaciente
+        agregarPaciente(nombrePaciente, telefonoPaciente)
+
+    #Opcion para Doctores con Especialidades
+    if(opcion == 3):
+        #Llamamos la funcion que se encarga de lo solicitado
+        getEspecialidades()
+
+    #Opcion para si una persona conoce a otra persona
+    if(opcion == 4):
+        #Llamaos a la funcion
         
 
-    if menu == "1" or menu == "ingresar nuevo doctor":
-        print ("ingrese un doctor")
-        
-    
-    if menu == "2" or menu == "ingresar nuevo paciente":
-        print ("ingrese un nuevo paciente")
-
-    if menu == "3" or menu == "doctores con tipos de especialidades":
-        print ("doctores con tipos de especialidades")
-
-    if menu == "4" or menu == "una persona conoce a otra":
-        print ("una persona conoce a otra")
-
+    #Opcion para recomendaciones de un paciente
+    if(opcion == 5):
 
      
     
