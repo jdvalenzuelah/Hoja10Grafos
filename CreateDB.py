0ab5a015ada370548b6b346c17211141086bbfe6
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-05-18 14:01:12 -0600

    Consultas db

--- CreateDB.py
+++ CreateDB.py
@@ -3,13 +3,14 @@
 
 #Import Graph Database
 from neo4jrestclient.client import GraphDatabase
+from neo4jrestclient import client
 
 #Connect to Neo4J server
 db = GraphDatabase("http://localhost:7474", username="neo4j", password="123456")
 
 #Creamos los "labels" (Tipo de dato)
 pacientes = db.labels.create("Patient")
-doctores = db.labels.create("Doctores")
+doctores = db.labels.create("Doctors")
 drogas = db.labels.create("Drugs")
 
 #Funcion para agregar el paciente
@@ -42,6 +43,32 @@ def visita(doctor, paciente):
 def conoce(persona1, persona2):
 	persona1.relationships.create("Knows", persona2)
 
+#Funcion para buscar la especialidades
+def getEspecialidades():
+	cypher = 'MATCH (n: Doctors) RETURN n ORDER BY n.esp'
+	query = db.query(cypher, returns=(client.Node, str, client.Node))
+	results = []
+	for result in query:
+		results.append(result[0]["esp"])
+	return eliminarRepetidos(results)
+
+#Funcion para elminar repetidos en consultas
+def eliminarRepetidos(list):
+	newList = []
+	for element in list:
+		if(element not in newList):
+			newList.append(element)
+	return newList
+
+#Funcion para buscar un doctor por especialidad
+def buscarDoctor(especialidad):
+	cypher = 'MATCH (n: Doctors) WHERE n.esp="' + especialidad +'" RETURN n'
+	query = db.query(cypher, returns=(client.Node, str, client.Node))
+	results = []
+	for result in query:
+		results.append("Nombre: %s Telefono: %s" % (result[0]["name"], result[0]["phone"]))
+	return results
+
 #Creacion de grafos de pruebas
 doc = agregarDoctor("Marcos", "545454", "Internista", "24353026")
 pac = agregarPaciente("Fernando", "35202766")
@@ -49,4 +76,6 @@ pac2 = agregarPaciente("David", "5454545")
 prescribe(doc, pac, "Aspririna", "17/05/2018", "25/05/2018", "cada 8h")
 visita(doc, pac)
 conoce(pac, pac2)
+for i in buscarDoctor("Internista"):
+	print(i)
 
